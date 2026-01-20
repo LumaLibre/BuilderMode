d3753555a3d60d2140fa9074e082fbb9b9340045
Jsinco <jonah@jsinco.dev>
2026-01-20 12:23:31 -0500

    Cleanup imports

--- src/main/java/dev/lumas/build/contexts/SuspendedContextCalculator.java
+++ src/main/java/dev/lumas/build/contexts/SuspendedContextCalculator.java
@@ -6,11 +6,11 @@ import net.luckperms.api.context.ContextConsumer;
 import net.luckperms.api.context.ContextSet;
 import net.luckperms.api.context.ImmutableContextSet;
 import org.bukkit.entity.Player;
-import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.NotNull;
 
 public class SuspendedContextCalculator implements ContextCalculator<Player> {
     @Override
-    public void calculate(@NonNull Player target, @NonNull ContextConsumer consumer) {
+    public void calculate(@NotNull Player target, @NotNull ContextConsumer consumer) {
         if (SuspendedPlayerRegistry.INSTANCE.isSuspended(target.getUniqueId())) {
             consumer.accept("suspended", "true");
         } else {
@@ -19,7 +19,7 @@ public class SuspendedContextCalculator implements ContextCalculator<Player> {
     }
 
     @Override
-    public @NonNull ContextSet estimatePotentialContexts() {
+    public @NotNull ContextSet estimatePotentialContexts() {
         return ImmutableContextSet.builder()
                 .add("suspended", "true")
                 .add("suspended", "false")
